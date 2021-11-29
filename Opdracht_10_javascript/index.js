5b8a11948c0ae02846cff416e580b596ac91b00b
boukelinker <92392365+boukelinker@users.noreply.github.com>
2021-11-29 20:09:49 +0100

    Add files via upload

---
+++ Opdracht_10_javascript/index.js
@@ -0,0 +1,22 @@
+console.log("Hello Winc Acadamy");
+let name = 'Bouke';
+console.log(name);
+
+let age = 4 + 4;
+let moi = '4' + '4';
+console.log(age);
+console.log(moi);
+
+let addition = 4 + 4;
+let substraction = 10 - 5;
+let multiplication = 5 * 5;
+let division = 500 / 5;
+let modulus = 18 % 4; //modulus is a remainder
+
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+
+

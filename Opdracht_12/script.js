7e77d9d4cf3c118bd7c494c7656278fdcfa44ca9
boukelinker <92392365+boukelinker@users.noreply.github.com>
2021-11-30 19:54:23 +0100

    Add files via upload

---
+++ Opdracht_12/script.js
@@ -0,0 +1,24 @@
+
+const age = 20;
+if (age >= 18) {
+    console.log("Welcome in the pub!");
+}
+else {
+    console.log("You're not old enough, please come back when you're old enough!")
+}
+
+const isFemale = false;
+if (isFemale == true) {
+    console.log("You can buy tickets for ladies night!")
+}
+else {
+    console.log("You can not buy tickets for ladies night...")
+}
+
+const driverStatus = "joost";
+if (driverStatus == "bob") {
+    console.log("You are allowed to drive. Drive safe!")
+}
+else {
+    console.log("Drinking and driving is not allowed, therefore you may not drive!!!")
+}

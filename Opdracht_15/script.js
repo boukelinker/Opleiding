60d4fb6b12963f0a259993326963eb61e1385a70
boukelinker <92392365+boukelinker@users.noreply.github.com>
2021-12-02 16:48:15 +0100

    Add files via upload

---
+++ Opdracht_15/script.js
@@ -0,0 +1,26 @@
+const makePizza = function () {
+    console.log("HOW TO MAKE PIZZA");
+    console.log("1. Make the dough");
+    console.log("2. Preheat the oven");
+    console.log("3. Role out the dough, in the shape of a circle");
+    console.log("4. Put some tomato sauce on the dough");
+    console.log("5. add some cheese");
+    console.log("6. Put it in the oven");
+};
+// doesn't work on it's own, needs to be called
+
+const makeSushi = function () {
+    console.log("HOW TO MAKE SUSHI");
+    console.log("1. cook rice");
+    console.log("2. cut fish");
+    console.log("3. put it all together");
+    console.log("4. wrap it in seaweed");
+};
+
+// calling the function
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();

abfa0876d5d6d199f32c8a1db27ea00ddece00bf
boukelinker <92392365+boukelinker@users.noreply.github.com>
2021-12-06 10:41:58 +0100

    Add files via upload

---
+++ Opdracht_20/even-odd-reporter.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 20; i++) {
+    //check if number is even
+    if (i % 2 == 0) {
+        evenOdd = "even"
+    }
+    //check if number is odd
+    else {
+        evenOdd = "odd"
+    }
+    console.log(i + " is " + evenOdd)
+}
---
+++ Opdracht_20/multiplication-tables.js
@@ -0,0 +1,15 @@
+/* 
+for (i = 1; i <= 10; i++) {
+    const multiplication = i * 9;
+    console.log(i + " x 9 = " + multiplication);
+};
+ */
+
+//Bonus
+for (i = 1; i <= 10; i++) {
+    console.log("Table of " + i)
+    for (x = 1; x <= 10; x++) {
+        multiplication = i * x;
+        console.log(i + " x " + x + " = " + multiplication)
+    };
+};
